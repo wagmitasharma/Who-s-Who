d6e2730954a3b6cb7a044c94998359166d57bc9e
wagmitasharma <wagmitasharma1998@gmail.com>
2021-07-15 11:25:11 +0530

    first commit

---
+++ src/main/java/com/example/whoswho/WhoSWhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.whoswho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoSWhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoSWhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whoswho/WhoSWhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whoswho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoSWhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
